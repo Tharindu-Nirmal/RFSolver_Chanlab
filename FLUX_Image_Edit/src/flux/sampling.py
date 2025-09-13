b0f58d76c354f7d4c9a8b21cde43dc90dd8c6d62
Tharindu-Nirmal <lwickrem@purdue.edu>
2025-09-13 12:01:56 -0400

    [clean] before lambda_t

--- FLUX_Image_Edit/src/flux/sampling.py
+++ FLUX_Image_Edit/src/flux/sampling.py
@@ -174,8 +174,8 @@ def denoise(
     # ddnm_list = [False] * (len(timesteps[:-1]) - info['ddnm_step']) + [True] * info['ddnm_step']
     
     # print('debug',len(timesteps[:-1])) #30 or 300
-    edit_count = 5 # last steps to do the edit
-    final_pad = 2 # last steps to skip ddnm
+    edit_count = 15 # last steps to do the edit
+    final_pad = 10 # last steps to skip ddnm
     ddnm_list =  [False]*(len(timesteps[:-1]) - edit_count) + [True]*(edit_count-final_pad) + [False]*(final_pad) 
     # ddnm_list =  [False]*(len(timesteps[:-1]))  #No DDNM update
     # print('debug',ddnm_list)
